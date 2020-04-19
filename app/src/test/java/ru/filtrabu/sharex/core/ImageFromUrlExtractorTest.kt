851092cd344ee296f124a45d927a01ade26bf115
Stanislav Zemlyakov <redtom@yandex.ru>
2020-04-19 11:04:49 +0300

    [ADDED] ktlint auto-format in each build

--- app/src/test/java/ru/filtrabu/sharex/core/ImageFromUrlExtractorTest.kt
+++ app/src/test/java/ru/filtrabu/sharex/core/ImageFromUrlExtractorTest.kt
@@ -1,10 +1,10 @@
 package ru.filtrabu.sharex.core
 
 import com.google.common.truth.Truth
-import org.junit.Test
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.util.stream.Collectors
+import org.junit.Test
 
 class ImageFromUrlExtractorTest {
 
