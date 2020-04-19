83678e49061f30d1019636ac30f1f97d42172894
Stanislav Zemlyakov <redtom@yandex.ru>
2020-04-19 18:54:13 +0300

    [ADDED] Nel logic for `extractJPG`

--- app/src/main/java/ru/filtrabu/sharex/core/ImageFromUrlExtractor.kt
+++ app/src/main/java/ru/filtrabu/sharex/core/ImageFromUrlExtractor.kt
@@ -1,27 +1,34 @@
 package ru.filtrabu.sharex.core
 
+import arrow.core.NonEmptyList
+import arrow.core.Option
 import org.jsoup.Jsoup.parse
 import org.jsoup.nodes.Document
 
 object ImageFromUrlExtractor {
 
     /**
-     * Accepts a HTML, and extracts all images contained in it,
-     * returning their URLs
+     * Accepts a HTML, and extracts all JPG files contained in it,
+     * returning their URLs, if present
+     *
+     * @return [NonEmptyList] of URLs if there are some JPEG links or sources, otherwise - [arrow.core.None]
      */
-    fun extractJPG(inputHTML: String): List<String> {
+    fun extractJPG(inputHTML: String): Option<NonEmptyList<String>> {
         val parseResult: Document = parse(inputHTML)
 
         // TODO: try getElementsByAttributeValueContains
+        val jpegFilter: (String) -> Boolean =
+            { it.contains("jpg") || it.contains("jpeg") }
         val filterJpgReferences = parseResult.getElementsByAttribute("href")
             .eachAttr("href")
-            .filter { it.contains("jpg") }
+            .filter(jpegFilter)
 
         val filterJpgSources =
             parseResult.getElementsByTag("img")
                 .eachAttr("src")
-                .filter { it.contains("jpg") }
+                .filter(jpegFilter)
 
-        return filterJpgReferences + filterJpgSources
+        val jpegs = filterJpgReferences + filterJpgSources
+        return arrow.core.Nel.fromList(jpegs)
     }
 }
