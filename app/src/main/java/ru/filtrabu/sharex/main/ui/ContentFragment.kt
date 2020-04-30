131f3f55f5140003252b18063228d8e6d992ef9f
Stanislav Zemlyakov <redtom@yandex.ru>
2020-04-30 20:54:56 +0300

    [MOVED] intentSubject subscription to Fragment

--- app/src/main/java/ru/filtrabu/sharex/main/ui/ContentFragment.kt
+++ app/src/main/java/ru/filtrabu/sharex/main/ui/ContentFragment.kt
@@ -1,10 +1,14 @@
 package ru.filtrabu.sharex.main.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Parcelable
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.hannesdorfmann.adapterdelegates4.dsl.adapterDelegateLayoutContainer
 import com.squareup.picasso.Picasso
+import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.fragment_content.*
 import kotlinx.android.synthetic.main.item_image.view.*
 import ru.filtrabu.sharex.R
@@ -12,6 +16,8 @@ import ru.filtrabu.sharex.R
 class ContentFragment : Fragment(R.layout.fragment_content) {
 
     data class ImageObject(val url: String) : ViewObject
+
+    private val cd: CompositeDisposable = CompositeDisposable()
 
     private val shareListAdapter = RecycleViewAdapter()
 
@@ -37,5 +43,45 @@ class ContentFragment : Fragment(R.layout.fragment_content) {
             addItemDecoration(OuterRecyclerViewItemDecoration)
             this.adapter = this@ContentFragment.shareListAdapter
         }
+
+        (requireActivity() as MainActivity).intentSubject
+            .subscribe(
+                { intent ->
+                    when (intent.action) {
+                        Intent.ACTION_SEND -> {
+                            when {
+                                intent.type == "text/plain" -> {
+                                    intent.getStringExtra(Intent.EXTRA_TEXT)?.let {
+                                        // FIXME: the url should be strictly acceptable by Picasso, need verification
+                                        shareListAdapter.items =
+                                            listOf(
+                                                ImageObject(it),
+                                                ImageObject(it),
+                                                ImageObject(it)
+                                            )
+                                        shareListAdapter.notifyDataSetChanged()
+                                    }
+                                }
+                                intent.type?.startsWith("image") == true -> {
+                                    (intent.getParcelableExtra<Parcelable>(Intent.EXTRA_STREAM) as? Uri)?.let {
+                                        // TODO("Update UI to reflect image being shared")
+                                    }
+                                }
+                                else -> {
+                                }
+                            }
+                        }
+                        Intent.ACTION_SEND_MULTIPLE -> {
+                            // TODO("Handle multiple ")
+                        }
+                        /* TODO: implement
+                        else -> showNothingToShare()
+                         */
+                    }
+                },
+                { error ->
+                    // TODO("Handle errors")
+                }
+            ).let { cd.add(it) }
     }
 }
--- app/src/main/java/ru/filtrabu/sharex/main/ui/MainActivity.kt
+++ app/src/main/java/ru/filtrabu/sharex/main/ui/MainActivity.kt
@@ -2,9 +2,7 @@ package ru.filtrabu.sharex.main.ui
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.os.Parcelable
 import androidx.fragment.app.FragmentActivity
 import io.reactivex.subjects.BehaviorSubject
 import ru.filtrabu.sharex.R
@@ -12,7 +10,7 @@ import ru.filtrabu.sharex.R
 class MainActivity : FragmentActivity() {
 
     // TODO: test (maybe BehaviorRelay is better)
-    private val intentSubject: BehaviorSubject<Intent> = BehaviorSubject.create()
+    val intentSubject: BehaviorSubject<Intent> = BehaviorSubject.create()
 
     @SuppressLint("CheckResult", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,38 +23,6 @@ class MainActivity : FragmentActivity() {
         } ?: run {
             showNothingToShare()
         }
-
-        intentSubject.subscribe({ intent ->
-            when (intent.action) {
-                Intent.ACTION_SEND -> {
-                    when {
-                        intent.type == "text/plain" -> {
-                            intent.getStringExtra(Intent.EXTRA_TEXT)?.let {
-                                // FIXME: the url should be strictly acceptable by Picasso, need verification
-                                /*
-                                shareListAdapter.items =
-                                    listOf(ImageObject(it), ImageObject(it), ImageObject(it))
-                                shareListAdapter.notifyDataSetChanged()
-                                 */
-                            }
-                        }
-                        intent.type?.startsWith("image") == true -> {
-                            (intent.getParcelableExtra<Parcelable>(Intent.EXTRA_STREAM) as? Uri)?.let {
-                                // TODO("Update UI to reflect image being shared")
-                            }
-                        }
-                        else -> {
-                        }
-                    }
-                }
-                Intent.ACTION_SEND_MULTIPLE -> {
-                    // TODO("Handle multiple ")
-                }
-                else -> showNothingToShare()
-            }
-        }, { error ->
-            // TODO("Handle errors")
-        })
     }
 
     @SuppressLint("SetTextI18n")
